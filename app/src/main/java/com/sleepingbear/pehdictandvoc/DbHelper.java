0726d75fdb4c084f4209a27f65b76767c2d8478e
limsm9449 <limsm9449@naver.com>
2016-11-24 14:48:20 +0900

    2016.11.24

--- app/src/main/java/com/sleepingbear/pehdictandvoc/DbHelper.java
+++ app/src/main/java/com/sleepingbear/pehdictandvoc/DbHelper.java
@@ -15,7 +15,7 @@ import java.io.OutputStream;
 class DbHelper extends SQLiteOpenHelper {
     private static String DB_NAME ="ehdictandvoc.db";
     private String DB_PATH = "";
-    private static final int DATABASE_VERSION = 92;
+    private static final int DATABASE_VERSION = 93;
     private final Context mContext;
 
     public DbHelper(Context context) {
